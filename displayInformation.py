da39a3a3a64fc2fecd0415a266fd005d7bf05376
Anh Pham Ngoc Phuong <npa@sofacompany.com>
2022-08-09 09:01:52 +0700

    Thông tin học viên

---
+++ displayInformation.py
@@ -0,0 +1,4 @@
+print("Họ và tên: Phạm Ngọc Phương Anh")
+print("Ngày sinh: 18/08/1987")
+print("Địa chỉ: 353/37/36 Đường TTH21, Phường Tân Thới Hiệp, Quận 12, Tp.HCM")
+print("Mục đích học Python: Mình muốn trở thành Data Analyst. Python là môn đầu tiên trong khóa học Data Analyst tại CodeGym")
